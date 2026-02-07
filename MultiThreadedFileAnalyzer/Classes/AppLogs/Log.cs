d43d7632bb21f428dec1fefc00404d4a44328b52
AlexPr06 <freezay00828002@gmail.com>
2026-02-07 23:23:46 +0200

    5. Bug fixed: Folder name Logs was changed to AppLogs in order to escape being put into .gitignore file.

---
+++ MultiThreadedFileAnalyzer/Classes/AppLogs/Log.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+using MultiThreadedFileAnalyzer.Interfaces;
+using Spectre.Console.Rendering;
+
+namespace MultiThreadedFileAnalyzer.Classes.Logs;
+
+internal sealed class LogItem : ILoggable
+{
+    public Color OutputColor { get; set; }
+    public string Message { get; set; }
+
+    public LogItem(string message, Color outputColor)
+    {
+        Message = message;
+        OutputColor = outputColor;
+    }
+
+    public IRenderable Log()
+    {
+        var panel = new Panel(Message)
+        {
+            Header = new PanelHeader("Log Entry"),
+            Border = BoxBorder.Rounded,
+            BorderStyle = new Style(OutputColor)
+        };
+        return panel;
+    }
+}
+
---
+++ MultiThreadedFileAnalyzer/Classes/AppLogs/LogPool.cs
@@ -0,0 +1,40 @@
+﻿using MultiThreadedFileAnalyzer.Interfaces;
+using System.Collections.Concurrent;
+
+namespace MultiThreadedFileAnalyzer.Classes.Logs;
+internal sealed class LogPool : ICleanable
+{
+    private readonly ConcurrentQueue<ILoggable> _loggableItems = new();
+
+    public void AddItem(ILoggable item) =>  _loggableItems.Enqueue(item);
+
+    public IReadOnlyCollection<ILoggable> AllLogs => _loggableItems;
+
+    public void ShowAll()
+    {
+        foreach (var item in _loggableItems)
+        {
+            item.Log();
+        }
+    }
+
+    public void ShowRecent(int count)
+    {
+        foreach (var item in _loggableItems.TakeLast(count))
+        {
+            item.Log();
+        }
+    }
+
+    public bool TryLogNext()
+    {
+        if (_loggableItems.TryDequeue(out var item))
+        {
+            item.Log();
+            return true;
+        }
+        return false;
+    }
+
+    public void Clean() => _loggableItems.Clear();
+}
